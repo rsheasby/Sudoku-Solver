d20317805888c68f25282bb4a6fd95f4dcdf4cb6
Ryan David Sheasby <ryan27968@gmail.com>
2017-10-11 16:08:06 +0200

    Added header files.

---
+++ include/backtrack.h
@@ -0,0 +1,6 @@
+#ifndef BACKTRACK_H
+# define BACKTRACK_H
+
+_Bool	backtrack(int x, int y);
+
+#endif
--- include/io.h
+++ include/io.h
@@ -2,6 +2,7 @@
 # define IO_H
 
 _Bool	readfile(char *filename);
+void	savesudoku(unsigned char board[9][9]);
 void	printsudoku(unsigned char board[9][9]);
 
 #endif
