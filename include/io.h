68c49454b96364eebe5c2305bb6b640acb34c719
Ryan David Sheasby <ryan27968@gmail.com>
2017-10-11 12:39:10 +0200

    Added IO

---
+++ include/io.h
@@ -0,0 +1,7 @@
+#ifndef FILEIO_H
+# define FILEIO_H
+
+_Bool	readfile(char *filename);
+void	printsudoku(unsigned char board[9][9]);
+
+#endif
---
+++ src/io.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <include/sudoku.h>
+#include <include/io.h>
+
+_Bool	readfile(char *filename)
+{
+	int		fd;
+	char	line[10];
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	for (int y = 0; y < 9; ++y)
+	{
+		read(fd, &line, 10);
+		for (int x = 0; x < 9; ++x)
+			if (line[x] >= '1' && line[x] <= '9')
+				g_globals.sudoku[y][x] = line[x] - 48;
+			else
+				g_globals.sudoku[y][x] = '\0';
+	}
+	close(fd);
+	return (1);
+}
+
+void	printsudoku(unsigned char board[9][9])
+{
+	for (int y = 0; y < 9; ++y)
+	{
+		if (y == 3 || y == 6)
+			printf("---+---+---\n");
+		for (int x = 0; x < 9; ++x)
+		{
+			if (x == 3 || x == 6)
+				printf("|");
+			if (!board[y][x])
+				printf(".");
+			else
+				printf("%d", board[y][x]);
+		}
+		printf("\n");
+	}
+}
